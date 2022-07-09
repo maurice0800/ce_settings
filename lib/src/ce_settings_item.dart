ee2fc61fc616ab90168840679b2cdba92e5bd011
Marvin Jäckisch <Marvin.Jaeckisch1@web.de>
2022-07-09 16:13:03 +0200

    Add onTap-functionality to items

--- lib/src/ce_settings_item.dart
+++ lib/src/ce_settings_item.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 class CESettingsItem extends CESettingsItemBase {
   final String text;
   final int badgeCount;
+  final VoidCallback? onTap;
 
   const CESettingsItem({
     Key? key,
@@ -13,68 +14,74 @@ class CESettingsItem extends CESettingsItemBase {
     bool lastItem = false,
     this.badgeCount = 0,
     bool showChevron = true,
+    this.onTap,
   }) : super(
           key: key,
           leading: leading,
           trailing: trailing,
           lastItem: lastItem,
           showChevron: showChevron,
+          onTap: onTap,
         );
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        if (leading != null)
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: leading,
-          ),
-        Expanded(
-          child: Container(
-            height: 44,
-            decoration: BoxDecoration(
-              border: !lastItem
-                  ? const Border(
-                      bottom: BorderSide(
-                          width: 1.0, color: CupertinoColors.separator),
-                    )
-                  : null,
+    return GestureDetector(
+      onTap: onTap,
+      child: Row(
+        children: [
+          if (leading != null)
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: leading,
             ),
-            child: Row(
-              children: [
-                Expanded(child: Text(text)),
-                if (badgeCount > 0)
-                  Stack(
-                    alignment: AlignmentDirectional.center,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(90.0),
-                          color: CupertinoColors.systemRed,
+          Expanded(
+            child: Container(
+              height: 44,
+              decoration: BoxDecoration(
+                border: !lastItem
+                    ? const Border(
+                        bottom: BorderSide(
+                            width: 1.0, color: CupertinoColors.separator),
+                      )
+                    : null,
+              ),
+              child: Row(
+                children: [
+                  Expanded(child: Text(text)),
+                  if (badgeCount > 0)
+                    Stack(
+                      alignment: AlignmentDirectional.center,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(90.0),
+                            color: CupertinoColors.systemRed,
+                          ),
+                          width: 22,
+                          height: 22,
                         ),
-                        width: 22,
-                        height: 22,
-                      ),
-                      Text(badgeCount.toString(),
-                          style: const TextStyle(color: CupertinoColors.white)),
-                    ],
-                  ),
-                trailing ?? Container(),
-                if (showChevron)
-                  const Padding(
-                    padding: EdgeInsets.only(left: 8.0),
-                    child: Icon(
-                      CupertinoIcons.chevron_right,
-                      color: CupertinoColors.inactiveGray,
-                      size: 20,
+                        Text(badgeCount.toString(),
+                            style:
+                                const TextStyle(color: CupertinoColors.white)),
+                      ],
                     ),
-                  ),
-              ],
+                  trailing ?? Container(),
+                  if (showChevron)
+                    const Padding(
+                      padding: EdgeInsets.only(left: 8.0),
+                      child: Icon(
+                        CupertinoIcons.chevron_right,
+                        color: CupertinoColors.inactiveGray,
+                        size: 20,
+                      ),
+                    ),
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
